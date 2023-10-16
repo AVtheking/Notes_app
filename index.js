f20f0374ad0eff107a7956e95603fd8823113162
Ankit Varshney <lyfflame45@gmail.com>
2023-10-16 19:05:53 +0530

    connection to mongodb done

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const mongoose = require("mongoose");
 const PORT = 3000;
 const app = express();
 
+//Database
+const DB =
+  "mongodb+srv://test:test123@cluster0.dhaqmmz.mongodb.net/?retryWrites=true&w=majority";
+
+//connection to Database
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connection is successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`connected at port ${PORT}`);
 });
